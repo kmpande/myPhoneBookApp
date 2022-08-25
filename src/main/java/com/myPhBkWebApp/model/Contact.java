cd597984daf9257f248bfdfbe62332bac92f2acb
prasannapande03 <prasannap2003@gmail.com>
2022-08-25 19:09:50 +0530

    MPHB-1 Create a Package Structure

---
+++ src/main/java/com/myPhBkWebApp/model/Contact.java
@@ -0,0 +1,50 @@
+package com.myPhBkWebApp.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Contact {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "contact_id")
+	private Integer contactId;
+	
+	@Column(name = "contact_name")
+	private String contactName;
+	
+	@Column(name = "contact_number")
+	private String contactNumber;
+	
+	@Column(name = "contact_email")
+	private String contactEmail;
+	
+	@Column(name= "active_switch")
+	private Character activeSwitch;
+	
+	@Column(name = "created_date", updatable = false)
+	@CreationTimestamp
+	private LocalDate createdDate;
+	
+	@Column(name = "updated_date" ,insertable = false)
+	@UpdateTimestamp
+	private LocalDate  updatedDate;
+}
---
+++ src/test/java/com/myPhBkWebApp/MyPhoneBookWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myPhBkWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhoneBookWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
