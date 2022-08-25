c3bb992f4977c37d4197bd9fefde1c9a9800f970
prasannapande03 <prasannap2003@gmail.com>
2022-08-25 20:48:45 +0530

    MPHB-3 Create a service and Service Impl

---
+++ src/main/java/com/myPhBkWebApp/service/ContactServiceI.java
@@ -0,0 +1,26 @@
+package com.myPhBkWebApp.service;
+
+import java.util.List;
+
+import com.myPhBkWebApp.model.Contact;
+
+
+
+public  interface ContactServiceI {
+	//create contact
+	Contact saveContact(Contact contact);
+	
+	//edit contact
+	Contact editContact(Contact contact, Integer contactId);
+	
+	// get a contact
+	Contact getContact(Integer contactid);
+	
+	//get all contacts
+	List<Contact> getAllContact();
+	
+	//delete contact
+	
+	void deleteContact(Integer contactId);
+	
+}
---
+++ src/main/java/com/myPhBkWebApp/service/ContactServiceImpl.java
@@ -0,0 +1,39 @@
+package com.myPhBkWebApp.service;
+
+import java.util.List;
+
+import com.myPhBkWebApp.model.Contact;
+
+public class ContactServiceImpl implements ContactServiceI{
+
+	@Override
+	public Contact saveContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact editContact(Contact contact, Integer contactId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact getContact(Integer contactid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Contact> getAllContact() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteContact(Integer contactId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
