0881c5d8f493e71bbe97cc5ecfa94d50ede3f1e3
prasannapande03 <prasannap2003@gmail.com>
2022-08-25 21:00:43 +0530

    MPHB-4 Create a repositroy

---
+++ src/main/java/com/myPhBkWebApp/repository/ContactRepo.java
@@ -0,0 +1,9 @@
+package com.myPhBkWebApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myPhBkWebApp.model.Contact;
+
+public interface ContactRepo extends JpaRepository<Contact, Integer> {
+
+}
