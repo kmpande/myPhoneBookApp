bd4ab0d1887b9d66654afc3ed4d103364df9ecdc
prasannapande03 <prasannap2003@gmail.com>
2022-08-27 21:35:38 +0530

    MPHB-12 create controller to save a contact

--- src/main/java/com/myPhBkWebApp/controller/ContactController.java
+++ src/main/java/com/myPhBkWebApp/controller/ContactController.java
@@ -1,9 +1,13 @@
 package com.myPhBkWebApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myPhBkWebApp.model.Contact;
 import com.myPhBkWebApp.service.ContactServiceI;
 
 @RestController
@@ -15,6 +19,12 @@ public class ContactController {
 	
 	//api to save a contact
 	
+	public ResponseEntity<Contact> saveContact(@RequestBody Contact contact){
+		Contact saveContact = contactServiceI.saveContact(contact);
+		
+		return new ResponseEntity<>(saveContact,HttpStatus.CREATED);
+		
+	}
 	//edit contact
 	
 	//get a contact by id
