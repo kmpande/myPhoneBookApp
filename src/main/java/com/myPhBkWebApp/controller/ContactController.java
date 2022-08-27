7b87c6b9ef1efa047c789c114a1b55ef5b7ac2c0
prasannapande03 <prasannap2003@gmail.com>
2022-08-27 21:42:11 +0530

    MPHB-13 Create controller to edit contact

--- src/main/java/com/myPhBkWebApp/controller/ContactController.java
+++ src/main/java/com/myPhBkWebApp/controller/ContactController.java
@@ -3,6 +3,8 @@ package com.myPhBkWebApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,14 @@ import com.myPhBkWebApp.model.Contact;
 import com.myPhBkWebApp.service.ContactServiceI;
 
 @RestController
-@RequestMapping("/api/contact/")
+@RequestMapping("/api/contact")
 public class ContactController {
 
 	@Autowired
 	private ContactServiceI contactServiceI;
 	
 	//api to save a contact
-	
+	@PostMapping("/")
 	public ResponseEntity<Contact> saveContact(@RequestBody Contact contact){
 		Contact saveContact = contactServiceI.saveContact(contact);
 		
@@ -26,7 +28,12 @@ public class ContactController {
 		
 	}
 	//edit contact
-	
+	public ResponseEntity<Contact> editContact(@RequestBody Contact contact, @PathVariable Integer contactId){
+		Contact editContact = contactServiceI.editContact(contact, contactId);
+		
+		return new ResponseEntity<Contact>(editContact, HttpStatus.OK);
+		
+	}
 	//get a contact by id
 	
 	//get all contacts
