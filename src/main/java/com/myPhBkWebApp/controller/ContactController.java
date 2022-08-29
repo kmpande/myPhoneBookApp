d3ba53bdaa15251dfc374d306820678cd81b2dcf
prasannapande03 <prasannap2003@gmail.com>
2022-08-29 19:49:31 +0530

    MPHB-14 create controller to get a contact by id

--- src/main/java/com/myPhBkWebApp/controller/ContactController.java
+++ src/main/java/com/myPhBkWebApp/controller/ContactController.java
@@ -3,6 +3,7 @@ package com.myPhBkWebApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +36,13 @@ public class ContactController {
 		
 	}
 	//get a contact by id
+	
+	@GetMapping("/{contactId}")
+	public ResponseEntity<Contact> getContactBYId(@PathVariable Integer contactId){
+		Contact contact = contactServiceI.getContact(contactId);
+		return ResponseEntity.ok(contact);
+		
+	}
 	
 	//get all contacts
 	
